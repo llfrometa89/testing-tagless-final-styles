b63d6d1b05af9b8a59851306fb9966e8a4238d55
Livan Frometa <lfrometa@latamautos.com>
2019-03-23 23:53:33 +0100

    Add generator instances

---
+++ src/main/scala/io/github/llfrometa89/domain/utils/Generator.scala
@@ -0,0 +1,5 @@
+package io.github.llfrometa89.domain.utils
+
+trait Generator[V] {
+  def generate: V
+}
---
+++ src/main/scala/io/github/llfrometa89/mtl_style/instances/GeneratorInstances.scala
@@ -0,0 +1,12 @@
+package io.github.llfrometa89.mtl_style.instances
+import io.github.llfrometa89.domain.utils.Generator
+
+import scala.util.Random
+
+trait GeneratorInstances {
+
+  implicit val accountNoGenerator: Generator[String] = new Generator[String] {
+    override def generate: String = new Random().nextInt(10000).toString
+  }
+
+}
